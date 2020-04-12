b4e6c7a203cb9ac5c18b716216e89927f3d9448e
Frank Haugen <frank.haugen@bilagos.no>
2020-04-12 21:37:48 +0200

    Added enumerable To Datatable extension

--- Frank.Extensions.Collections/IEnumerableExtensions.cs
+++ Frank.Extensions.Collections/IEnumerableExtensions.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
+using System.Reflection;
 
 namespace Frank.Extensions.Collections
 {
@@ -21,6 +23,31 @@ namespace Frank.Extensions.Collections
             var index = random.Next(0, count);
 
             return items.ElementAt(index);
+        }
+
+        public static DataTable ToDataTable<T>(this IEnumerable<T> items)
+        {
+            var tb = new DataTable(typeof(T).Name);
+
+            var props = typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+
+            foreach (var prop in props)
+            {
+                tb.Columns.Add(prop.Name, prop.PropertyType);
+            }
+
+            foreach (var item in items)
+            {
+                var values = new object[props.Length];
+                for (var i = 0; i < props.Length; i++)
+                {
+                    values[i] = props[i].GetValue(item, null);
+                }
+
+                tb.Rows.Add(values);
+            }
+
+            return tb;
         }
     }
 }
