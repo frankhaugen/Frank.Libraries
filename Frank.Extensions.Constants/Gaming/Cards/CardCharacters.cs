b15760757d962351a806413e34aa5d83eab63ab9
Frank Haugen <frank.haugen@bilagos.no>
2020-04-13 03:13:20 +0200

    Added constants-project and more enums

---
+++ Frank.Extensions.Constants/Gaming/Cards/CardCharacters.cs
@@ -0,0 +1,77 @@
+﻿namespace Frank.Extensions.Constants.Gaming.Cards
+{
+    public static class CardCharacters
+    {
+        public static class Others
+        {
+            public static readonly string Back = "\U0001F0A0";
+            public static readonly string Joker = "\U0001F0BF";
+        }
+
+        public static class Spades
+        {
+            public static readonly string Ace = "\U0001F0A1";
+            public static readonly string Two = "\U0001F0A2";
+            public static readonly string Three = "\U0001F0A3";
+            public static readonly string Four = "\U0001F0A4";
+            public static readonly string Five = "\U0001F0A5";
+            public static readonly string Six = "\U0001F0A6";
+            public static readonly string Seven = "\U0001F0A7";
+            public static readonly string Eight = "\U0001F0A8";
+            public static readonly string Nine = "\U0001F0A9";
+            public static readonly string Ten = "\U0001F0AA";
+            public static readonly string Jack = "\U0001F0AB";
+            public static readonly string Queen = "\U0001F0AC";
+            public static readonly string King = "\U0001F0AD";
+        }
+
+        public static class Hearts
+        {
+            public static readonly string Ace = "\U0001F0B1";
+            public static readonly string Two = "\U0001F0B2";
+            public static readonly string Three = "\U0001F0B3";
+            public static readonly string Four = "\U0001F0B4";
+            public static readonly string Five = "\U0001F0B5";
+            public static readonly string Six = "\U0001F0B6";
+            public static readonly string Seven = "\U0001F0B7";
+            public static readonly string Eight = "\U0001F0B8";
+            public static readonly string Nine = "\U0001F0B9";
+            public static readonly string Ten = "\U0001F0BA";
+            public static readonly string Jack = "\U0001F0BB";
+            public static readonly string Queen = "\U0001F0BC";
+            public static readonly string King = "\U0001F0BD";
+        }
+        public static class Diamonds
+        {
+            public static readonly string Ace = "\U0001F0C1";
+            public static readonly string Two = "\U0001F0C2";
+            public static readonly string Three = "\U0001F0C3";
+            public static readonly string Four = "\U0001F0C4";
+            public static readonly string Five = "\U0001F0C5";
+            public static readonly string Six = "\U0001F0C6";
+            public static readonly string Seven = "\U0001F0C7";
+            public static readonly string Eight = "\U0001F0C8";
+            public static readonly string Nine = "\U0001F0C9";
+            public static readonly string Ten = "\U0001F0CA";
+            public static readonly string Jack = "\U0001F0CB";
+            public static readonly string Queen = "\U0001F0CC";
+            public static readonly string King = "\U0001F0CD";
+        }
+        public static class Clubs
+        {
+            public static readonly string Ace = "\U0001F0D1";
+            public static readonly string Two = "\U0001F0D2";
+            public static readonly string Three = "\U0001F0D3";
+            public static readonly string Four = "\U0001F0D4";
+            public static readonly string Five = "\U0001F0D5";
+            public static readonly string Six = "\U0001F0D6";
+            public static readonly string Seven = "\U0001F0D7";
+            public static readonly string Eight = "\U0001F0D8";
+            public static readonly string Nine = "\U0001F0D9";
+            public static readonly string Ten = "\U0001F0DA";
+            public static readonly string Jack = "\U0001F0DB";
+            public static readonly string Queen = "\U0001F0DC";
+            public static readonly string King = "\U0001F0DD";
+        }
+    }
+}
