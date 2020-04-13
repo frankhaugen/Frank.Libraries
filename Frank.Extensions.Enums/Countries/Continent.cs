4d672161447dad02ed9c80c7e2ad912f6b26aa8c
Frank Haugen <frank.haugen@bilagos.no>
2020-04-13 02:47:48 +0200

    Added enums

---
+++ Frank.Extensions.Enums/Countries/Continent.cs
@@ -0,0 +1,13 @@
+﻿namespace Frank.Extensions.Enums.Countries
+{
+    public enum Continent
+    {
+        Africa,
+        Antarctica,
+        Asia,
+        Europe,
+        Oceania,
+        NorthAmerica,
+        SouthAmerica
+    }
+}
---
+++ Frank.Extensions.Enums/Science/Astronomy/Planet.cs
@@ -0,0 +1,14 @@
+﻿namespace Frank.Extensions.Enums.Science.Astronomy
+{
+    public enum Planet
+    {
+        Mercury = 1,
+        Venus = 2,
+        Earth = 3,
+        Mars = 4,
+        Jupiter = 5,
+        Saturn = 6,
+        Uranus = 7,
+        Neptune = 8
+    }
+}
