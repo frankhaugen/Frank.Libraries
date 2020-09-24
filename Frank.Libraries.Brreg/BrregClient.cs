f8eabf7db260e9098fbe03ac27c1f964fc57ec7e
Kiza <belteksllc@gmail.com>
2020-09-24 09:04:39 +0200

    ADD: Ability to search BRREG by company name and town and to retrieve list of companies found

--- Frank.Libraries.Brreg/BrregClient.cs
+++ Frank.Libraries.Brreg/BrregClient.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Threading.Tasks;
 using Flurl;
 using Flurl.Http;
@@ -10,6 +8,9 @@ namespace Frank.Libraries.Brreg
     public class BrregClient
     {
         private string BrregUrl(long organizationNumber) => $"https://data.brreg.no/enhetsregisteret/api/enheter/{organizationNumber}";
+
+        private string BrregSearchUrl(string name, string town, int currentPage, int pageSize) => $"https://data.brreg.no/enhetsregisteret/api/enheter?navn={name}&postadresse.poststed={town}&page={currentPage}&size={pageSize}";
+
 
         public async Task<Company> GetCompanyAsync(long organizationNumber)
         {
@@ -21,6 +22,38 @@ namespace Frank.Libraries.Brreg
             }
 
             return await url.GetJsonAsync<Company>();
+        }
+
+        public async Task<PaginatedResponse<CustomerBrregResponse>> SearchForLegalEntityAsync(PaginatedBrregSearch queryString)
+        {
+
+            var url = new Url(BrregSearchUrl(queryString.Name, queryString.Town, queryString.CurrentPage - 1, queryString.PageSize));
+            var response = await url.GetJsonAsync<LegalEntityPaginated>();
+
+            var paginated = new PaginatedResponse<CustomerBrregResponse>();
+
+            //Mapp to CustomerBrregResponse
+            var legalEntities = new List<CustomerBrregResponse>();
+            if (response.Data != null)
+            {
+                foreach (var legalEntity in response.Data.LegalEntities)
+                {
+                    legalEntities.Add(new CustomerBrregResponse(legalEntity));
+                }
+            }
+
+            paginated.Data = legalEntities;
+            var meta = new PaginatedMeta();
+
+            meta.PageSize = response.Page.size;
+            meta.CurrentPage = response.Page.number + 1;
+            meta.TotalCount = response.Page.totalElements;
+            meta.TotalPages = response.Page.totalPages;
+
+            paginated.Meta = meta;
+
+
+            return paginated;
         }
     }
 }
