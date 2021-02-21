2639a46b4059c47e0366c05d8ca2daa95037b930
Frank R. Haugen <frank.haugen@gmail.com>
2021-02-21 22:37:18 +0100

    Added initial mapping package

---
+++ Frank.Libraries.Extensions/UriExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Frank.Libraries.Extensions
+{
+    public static class UriExtensions
+    {
+        public static Uri AddSegment(this Uri uri, string segment)
+        {
+            return new Uri(uri, segment);
+        }
+    }
+}
---
+++ Frank.Libraries.Json/README.md
@@ -0,0 +1,2 @@
+﻿# Frank.Libraries.Json
+
