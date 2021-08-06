c549b3b9c5eb316ec0226e29922ce46535618bbb
Frank R. Haugen <frank.haugen@gmail.com>
2021-08-06 20:25:15 +0200

    Updated nugets and cleaned MachineInfo

--- Frank.Libraries.Machine/MachineInfo.cs
+++ Frank.Libraries.Machine/MachineInfo.cs
@@ -1,10 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.PlatformServices;
+using HidSharp;
 
 namespace Frank.Libraries.Machine
 {
     public class MachineInfo
     {
-        public string GetName() => Environment.MachineName;
+        public string GetName() => System.Environment.MachineName;
 
         //public bool CheckPlatform(PlatformID platform) => GetPlatform() == platform;
 
@@ -12,12 +16,37 @@ namespace Frank.Libraries.Machine
 
         public string GetUserName() => Environment.UserName;
 
+        public string GetMachineName() => Environment.MachineName;
+
         public Version GetOsVersion() => Environment.OSVersion.Version;
 
-        //public PlatformID GetPlatform() => Environment.OSVersion.Platform;
+        public OperatingSystem GetOperatingSystem() => Environment.OSVersion;
+
+        public PlatformID GetPlatformId() => Environment.OSVersion.Platform;
 
         public bool Is64Bit() => Environment.Is64BitOperatingSystem;
 
         public TimeSpan Uptime() => TimeSpan.FromTicks(Environment.TickCount64);
+
+        public List<Device> GetDeviceList() => DeviceList.Local.GetAllDevices().ToList();
+
+        public DateTimeOffset GetSystemTime() => SystemClock.UtcNow;
+    }
+
+    public class DeviceInfo
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string FriendlyName { get; set; }
+    }
+
+    public static class DeviceExtension
+    {
+        public static DeviceInfo GetDeviceInfo(this Device device) => new()
+        {
+            Id = device.DevicePath,
+            Name = device.GetFileSystemName(),
+            FriendlyName = device.GetFriendlyName()
+        };
     }
 }
