6b7ab0920191bb8e474df4d7ef5b878b14621cc4
Frank R. Haugen <frank.haugen@gmail.com>
2021-10-31 02:50:43 +0200

    Added a bunch of tests

---
+++ src/Frank.Libraries.Tests/FluentCalculation/FluentCalculatorExponentialOperationsTests.cs
@@ -0,0 +1,257 @@
+﻿using Frank.Libraries.Calculators.FluentCalculation;
+using Frank.Libraries.Tests.TestingInfrastructure;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Frank.Libraries.Tests.FluentCalculation
+{
+    public class FluentCalculatorExponentialOperationsTests : TestBase
+    {
+        [Fact]
+        public void Pow_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            int source = 0;
+            int value = 0;
+
+            // Act
+            var result = source.Pow(value);
+
+            // Assert
+            Output(result);
+        }
+
+        [Fact]
+        public void Sqrt_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            int source = 0;
+
+            // Act
+            var result = source.Sqrt();
+
+            // Assert
+            Output(result);
+        }
+
+        [Fact]
+        public void Pow_StateUnderTest_ExpectedBehavior1()
+        {
+            // Arrange
+            uint source = 0;
+            int value = 0;
+
+            // Act
+            var result = source.Pow(value);
+
+            // Assert
+            Output(result);
+        }
+
+        [Fact]
+        public void Sqrt_StateUnderTest_ExpectedBehavior1()
+        {
+            // Arrange
+            uint source = 0;
+
+            // Act
+            var result = source.Sqrt();
+
+            // Assert
+            Output(result);
+        }
+
+        [Fact]
+        public void Pow_StateUnderTest_ExpectedBehavior2()
+        {
+            // Arrange
+            short source = 0;
+            int value = 0;
+
+            // Act
+            var result = source.Pow(value);
+
+            // Assert
+            Output(result);
+        }
+
+        [Fact]
+        public void Sqrt_StateUnderTest_ExpectedBehavior2()
+        {
+            // Arrange
+            short source = 0;
+
+            // Act
+            var result = source.Sqrt();
+
+            // Assert
+            Output(result);
+        }
+
+        [Fact]
+        public void Pow_StateUnderTest_ExpectedBehavior3()
+        {
+            // Arrange
+            ushort source = 0;
+            int value = 0;
+
+            // Act
+            var result = source.Pow(value);
+
+            // Assert
+            Output(result);
+        }
+
+        [Fact]
+        public void Sqrt_StateUnderTest_ExpectedBehavior3()
+        {
+            // Arrange
+            ushort source = 0;
+
+            // Act
+            var result = source.Sqrt();
+
+            // Assert
+            Output(result);
+        }
+
+        [Fact]
+        public void Pow_StateUnderTest_ExpectedBehavior4()
+        {
+            // Arrange
+            long source = 0;
+            int value = 0;
+
+            // Act
+            var result = source.Pow(value);
+
+            // Assert
+            Output(result);
+        }
+
+        [Fact]
+        public void Sqrt_StateUnderTest_ExpectedBehavior4()
+        {
+            // Arrange
+            long source = 0;
+
+            // Act
+            var result = source.Sqrt();
+
+            // Assert
+            Output(result);
+        }
+
+        [Fact]
+        public void Pow_StateUnderTest_ExpectedBehavior5()
+        {
+            // Arrange
+            ulong source = 0;
+            int value = 0;
+
+            // Act
+            var result = source.Pow(value);
+
+            // Assert
+            Output(result);
+        }
+
+        [Fact]
+        public void Sqrt_StateUnderTest_ExpectedBehavior5()
+        {
+            // Arrange
+            ulong source = 0;
+
+            // Act
+            var result = source.Sqrt();
+
+            // Assert
+            Output(result);
+        }
+
+        [Fact]
+        public void Pow_StateUnderTest_ExpectedBehavior6()
+        {
+            // Arrange
+            float source = 0;
+            int value = 0;
+
+            // Act
+            var result = source.Pow(value);
+
+            // Assert
+            Output(result);
+        }
+
+        [Fact]
+        public void Sqrt_StateUnderTest_ExpectedBehavior6()
+        {
+            // Arrange
+            float source = 0;
+
+            // Act
+            var result = source.Sqrt();
+
+            // Assert
+            Output(result);
+        }
+
+        [Fact]
+        public void Pow_StateUnderTest_ExpectedBehavior7()
+        {
+            // Arrange
+            double source = 0;
+            int value = 0;
+
+            // Act
+            var result = source.Pow(value);
+
+            // Assert
+            Output(result);
+        }
+
+        [Fact]
+        public void Sqrt_StateUnderTest_ExpectedBehavior7()
+        {
+            // Arrange
+            double source = 0;
+
+            // Act
+            var result = source.Sqrt();
+
+            // Assert
+            Output(result);
+        }
+
+        [Fact]
+        public void Pow_StateUnderTest_ExpectedBehavior8()
+        {
+            // Arrange
+            decimal source = 0;
+            int value = 0;
+
+            // Act
+            var result = source.Pow(value);
+
+            // Assert
+            Output(result);
+        }
+
+        [Fact]
+        public void Sqrt_StateUnderTest_ExpectedBehavior8()
+        {
+            // Arrange
+            decimal source = 0;
+
+            // Act
+            var result = source.Sqrt();
+
+            // Assert
+            Output(result);
+        }
+
+        public FluentCalculatorExponentialOperationsTests(ITestOutputHelper outputHelper) : base(outputHelper)
+        {
+        }
+    }
+}
---
+++ src/Frank.Libraries.Tests/FluentCalculation/FluentCalculatorGeometricCalculationTests.cs
@@ -0,0 +1,114 @@
+﻿using System.Numerics;
+using Frank.Libraries.Calculators.FluentCalculation;
+using Frank.Libraries.Calculators.Types;
+using Frank.Libraries.Tests.TestingInfrastructure;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Frank.Libraries.Tests.FluentCalculation
+{
+    public class FluentCalculatorGeometricCalculationTests : TestBase
+    {
+        public FluentCalculatorGeometricCalculationTests(ITestOutputHelper outputHelper) : base(outputHelper)
+        {
+        }
+
+        [Fact]
+        public void ToPolar_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            CartesianCoordinates coordinates = new CartesianCoordinates() { X = 1, Y = 1, Z = 1 };
+
+            // Act
+            var result = coordinates.ToPolar();
+
+            // Assert
+            Output(result);
+        }
+
+        [Fact]
+        public void ToCartesian_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            PolarCoordinates polarCoordinates = default(PolarCoordinates);
+
+            // Act
+            var result = polarCoordinates.ToCartesian();
+
+            // Assert
+            Output(result);
+        }
+
+        [Fact]
+        public void Distance_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            Vector2 origin = default(Vector2);
+            Vector2 destination = default(Vector2);
+
+            // Act
+            var result = origin.Distance(destination);
+
+            // Assert
+            Output(result);
+        }
+
+        [Fact]
+        public void Distance_StateUnderTest_ExpectedBehavior1()
+        {
+            // Arrange
+            Vector3 origin = default(Vector3);
+            Vector3 destination = default(Vector3);
+
+            // Act
+            var result = origin.Distance(destination);
+
+            // Assert
+            Output(result);
+            Output(result);
+        }
+
+        [Fact]
+        public void Distance_StateUnderTest_ExpectedBehavior2()
+        {
+            // Arrange
+            CartesianCoordinates origin = default(CartesianCoordinates);
+            CartesianCoordinates destination = default(CartesianCoordinates);
+
+            // Act
+            var result = origin.Distance(destination);
+
+            // Assert
+            Output(result);
+        }
+
+        [Fact]
+        public void Distance_StateUnderTest_ExpectedBehavior3()
+        {
+            // Arrange
+            Vector3 origin = default(Vector3);
+            CartesianCoordinates destination = default(CartesianCoordinates);
+
+            // Act
+            var result = origin.Distance(destination);
+
+            // Assert
+            Output(result);
+        }
+
+        [Fact]
+        public void Distance_StateUnderTest_ExpectedBehavior4()
+        {
+            // Arrange
+            CartesianCoordinates origin = default(CartesianCoordinates);
+            Vector3 destination = default(Vector3);
+
+            // Act
+            var result = origin.Distance(destination);
+
+            // Assert
+            Output(result);
+        }
+
+    }
+}
