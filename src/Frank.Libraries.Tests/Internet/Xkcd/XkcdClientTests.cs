a2374f7d5a5f914fede5109f4f49a8d42703b406
Frank R. Haugen <frank.haugen@gmail.com>
2022-01-03 18:32:18 +0100

    Added some logic

--- src/Frank.Libraries.Tests/Internet/Xkcd/XkcdClientTests.cs
+++ src/Frank.Libraries.Tests/Internet/Xkcd/XkcdClientTests.cs
@@ -1,10 +1,13 @@
+using System.IO;
+using System.Linq;
 using FluentAssertions;
 using Frank.Libraries.Extensions;
 using Frank.Libraries.Internet.Xkcd;
 using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.CSharp;
 using NJsonSchema.CodeGeneration.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using NSwag;
-using NSwag.CodeGeneration;
 using NSwag.CodeGeneration.CSharp;
 using Xunit;
 using Xunit.Abstractions;
@@ -55,6 +58,106 @@ public class XkcdClientTests
         // Assert
         _outputHelper.WriteLine(result);
         result.ToString().Should().NotBeNullOrEmpty();
+    }
+
+    [Fact]
+    public async Task Something2()
+    {
+        var document = await OpenApiDocument.FromUrlAsync("https://esi.evetech.net/latest/swagger.json");
+        var @namespace = "EveOnlineApi";
+        var settings = new CSharpClientGeneratorSettings()
+        {
+	        ClassName = "Test",
+	        WrapResponses = false,
+	        GenerateDtoTypes = true,
+	        GenerateExceptionClasses = false,
+	        WrapDtoExceptions = false,
+	        GenerateClientClasses = false,
+	        GenerateOptionalParameters = true,
+	        CSharpGeneratorSettings =
+			        {
+				        Namespace = @namespace,
+				        ClassStyle = CSharpClassStyle.Record,
+				        ArrayType = "IList",
+				        JsonLibrary = CSharpJsonLibrary.SystemTextJson,
+				        GenerateNativeRecords = true,
+				        GenerateDefaultValues = false,
+				        GenerateNullableReferenceTypes = true,
+				        GenerateDataAnnotations = false
+			        }
+        };
+
+        var outputDirectory = new DirectoryInfo(Path.Combine(@"C:\repos\frankhaugen\Frank.Libraries\src\Frank.Libraries.Gaming\Engine", @namespace));
+        if (!outputDirectory.Exists)
+        {
+	        outputDirectory.Create();
+        }
+
+        var generator = new CSharpClientGenerator(document, settings);
+        var text = generator.GenerateFile();
+        //var text = File.ReadAllText(@"C:\temp\SwaggerGeneratedCode.txt");
+
+        var syntaxTree = CSharpSyntaxTree.ParseText(text);
+        var root = syntaxTree.GetRoot() as CompilationUnitSyntax;
+        var namespaceSyntax = root.Members.OfType<NamespaceDeclarationSyntax>().First();
+        var classes = namespaceSyntax.Members.OfType<ClassDeclarationSyntax>();
+        var records = namespaceSyntax.Members.OfType<RecordDeclarationSyntax>();
+        var enums = namespaceSyntax.Members.OfType<EnumDeclarationSyntax>();
+
+        //var classesDictionary = classes.ToDictionary(x => x.Identifier.ToString(), x => x);
+        //var recordsDictionary = records.ToDictionary(x => x.Identifier.ToString(), x => x);
+        //var enumsDictionary = enums.ToDictionary(x => x.Identifier.ToString(), x => x);
+
+        foreach (var element in classes)
+        {
+	        var outputPath = Path.Combine(outputDirectory.FullName, element.Identifier.ToString() + ".cs");
+	        var outputFile = new FileInfo(outputPath);
+
+	        if (outputFile.Exists)
+	        {
+		        outputPath = Path.Combine(outputDirectory.FullName, element.Identifier.ToString() + "X" + ".cs");
+		        outputFile = new FileInfo(outputPath);
+	        }
+
+            _outputHelper.WriteLine($"{element.Identifier.ToString()}\t=>\t{outputFile.Name}");
+	        if (outputFile.Exists) continue;
+
+        //	Console.WriteLine($"{element.Identifier.ToString()}\t=>\t{outputFile.Name}");
+        //	File.WriteAllText(outputFile.FullName, $"namespace {namespaceSyntax.Name};\n" + element.ToFullString().Replace(" partial ", " "));
+        }
+        foreach (var element in records)
+        {
+	        var outputPath = Path.Combine(outputDirectory.FullName, element.Identifier.ToString() + ".cs");
+	        var outputFile = new FileInfo(outputPath);
+
+	        if (outputFile.Exists)
+	        {
+		        outputPath = Path.Combine(outputDirectory.FullName, element.Identifier.ToString() + "X" + ".cs");
+		        outputFile = new FileInfo(outputPath);
+	        }
+            _outputHelper.WriteLine($"{element.Identifier.ToString()}\t=>\t{outputFile.Name}");
+
+	        if (outputFile.Exists) continue;
+
+        //	Console.WriteLine($"{element.Identifier.ToString()}\t=>\t{outputFile.Name}");
+        //	File.WriteAllText(outputFile.FullName, $"namespace {namespaceSyntax.Name};\n" + element.ToFullString().Replace(" partial ", " "));
+        }
+        foreach (var element in enums)
+        {
+	        var outputPath = Path.Combine(outputDirectory.FullName, element.Identifier.ToString() + ".cs");
+	        var outputFile = new FileInfo(outputPath);
+
+	        if (outputFile.Exists)
+	        {
+		        outputPath = Path.Combine(outputDirectory.FullName, element.Identifier.ToString() + "X" + ".cs");
+		        outputFile = new FileInfo(outputPath);
+	        }
+
+            _outputHelper.WriteLine($"{element.Identifier.ToString()}\t=>\t{outputFile.Name}");
+	        if (outputFile.Exists) continue;
+        //	Console.WriteLine($"{element.Identifier.ToString()}\t=>\t{outputFile.Name}");
+        //	File.WriteAllText(outputFile.FullName, $"namespace {namespaceSyntax.Name};\n" + element.ToFullString().Replace(" partial ", " "));
+        }
     }
 
     [Fact]
